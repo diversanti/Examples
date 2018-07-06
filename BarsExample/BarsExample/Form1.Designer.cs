10c11735c84a1623156a76c8b4b701f345254425
diversanti <iliabibiluri@gmail.com>
2018-07-06 22:49:37 +0400

    s

---
+++ BarsExample/BarsExample/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+
+namespace BarsExample
+{
+    partial class Form1
+    {
+
+        private Random rnd = new Random();
+        private int rectSpace = 10;
+        private int maxHeight = 400;
+        private int rectWidth = 0;
+        private Point startingPoint = new Point();
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(312, 12);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(158, 50);
+            this.inputTextBox.TabIndex = 0;
+            this.inputTextBox.TextChanged += new System.EventHandler(this.inputTextBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+    }
+}
+
---
+++ BarsExample/BarsExample/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BarsExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            this.Refresh();
+            if (inputTextBox.Text.Equals("") || inputTextBox.Text == null)
+                return;
+            startingPoint.X = 10;
+            startingPoint.Y = this.Height - 10;
+            int rectsAmount = int.Parse(inputTextBox.Text);
+            rectWidth = (this.Width - rectSpace * (rectsAmount + 1)) / rectsAmount;
+            for (int i = 0; i < rectsAmount; i++)
+            {
+                Color randomColor = Color.FromArgb(rnd.Next(10,200), rnd.Next(10,200), rnd.Next(30,200));
+                System.Drawing.SolidBrush myBrush = new System.Drawing.SolidBrush(randomColor);
+                System.Drawing.Graphics formGraphics;
+                formGraphics = this.CreateGraphics();
+                Rectangle rect = new Rectangle();
+                int rectHeight = rnd.Next(35, maxHeight);
+                rect.Location = new Point(startingPoint.X + i * (rectWidth + rectSpace), startingPoint.Y - rectHeight);
+                rect.Size = new Size(rectWidth, rectHeight);
+                formGraphics.FillRectangle(myBrush, rect);
+                myBrush.Dispose();
+                formGraphics.Dispose();
+            }
+        }
+    }
+}
+
